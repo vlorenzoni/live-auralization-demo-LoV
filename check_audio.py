a10075dab2e083f2a26760aa58240f884038d189
vlorenzoni <riobale@gmail.com>
2026-05-07 11:36:41 +0200

    auralization files etc.

--- check_audio.py
+++ check_audio.py
@@ -1,61 +1,29 @@
+import os
+
 import soundfile as sf
 import matplotlib.pyplot as plt
 
+# get all names of the files in the folder
 
-#read the audio file
-data, samplerate = sf.read('filter_nassau_X7.wav')
-print(f"Audio data shape: {data.shape}")
-print(f"Sample rate: {samplerate}")
+auralization_files = [
+    f for f in os.listdir("auralization_filters/") if f.endswith(".wav")
+]
 
-print("Audio data type:", data.dtype)
-#print max and min values to check if they are in the expected range
-print(f"Max value: {data.max()}")
-print(f"Min value: {data.min()}")
-
-#plotting the first 1000 samples to visualize the audio data
-plt.figure(figsize=(12, 6)) 
-# plot all samples of the audio data
-plt.plot(data[::])  # 
-plt.xlabel("Sample")
+folder_path = "auralization_filters/"
 
 
-
-
-#read other audio file
-data2, samplerate2 = sf.read('filters_SDM_Brou_small_room_pos_LS1_MS1.mat.wav')
-print(f"Audio data shape: {data2.shape}")
-print(f"Sample rate: {samplerate2}")
-
-print("Audio data type:", data2.dtype)
-#print max and min values to check if they are in the expected range
-print(f"Max value: {data2.max()}")
-print(f"Min value: {data2.min()}")
-
-#plotting the first 1000 samples to visualize the audio data
-plt.figure(figsize=(12, 6))
-plt.plot(data2[::])  # plot the first 3 seconds of audio
-plt.xlabel("Sample")
-
-
-#read other audio file
-data3, samplerate3 = sf.read('filters_Brou_small_room_pos_LS1_MS1.wav')
-print(f"Audio data shape: {data3.shape}")
-print(f"Sample rate: {samplerate3}")
-
-print("Audio data type:", data3.dtype)
-#print max and min values to check if they are in the expected range
-print(f"Max value: {data3.max()}")
-print(f"Min value: {data3.min()}")
-
-#plotting the first 1000 samples to visualize the audio data
-plt.figure(figsize=(12, 6))
-plt.plot(data3[::])  # plot the first 3 seconds of audio
-plt.xlabel("Sample")
-
-
-plt.show()
-
-
+for file in auralization_files:
+    file_path = os.path.join(folder_path, file)
+    data, samplerate = sf.read(file_path)
+    print(f"File: {file}, Sample Rate: {samplerate}, Duration: {len(data)/samplerate:.2f} seconds")
+    print(f"File: {file}, Size of data: {len(data)} samples")   
+    plt.figure(figsize=(10, 4))
+    plt.plot(data)
+    plt.title(f"Waveform of {file}")
+    plt.xlabel("Sample Index")
+    plt.ylabel("Amplitude")
+    print(f"File: {file}, Sample Rate: {samplerate}, Duration: {len(data)/samplerate:.2f} seconds")
+    
 
 
 
